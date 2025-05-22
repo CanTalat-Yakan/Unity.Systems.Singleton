73eb8cffc077b3ac1d7c42c2135c8425ef0ec978
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-22 19:37:15 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,87 +1,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : Component
+namespace UnityEssentials
 {
-    public static bool HasInstance => _instance != null;
-    public static T TryGetInstance() => HasInstance ? _instance : null;
-    public static T Current => _instance;
-
-    public static T Instance
+    public class Singleton<T> : MonoBehaviour where T : Component
     {
-        get
+        public static bool HasInstance => _instance != null;
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+        public static T Current => _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new();
+                        obj.name = typeof(T).Name + "AutoCreated";
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public static T _instance { get; set; }
+
+        public virtual void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
+        }
+
+        public virtual void Awake()
+        {
+            if (Application.isPlaying)
+                InitializeSingleton();
+        }
+
+        public virtual void InitializeSingleton() =>
+            _instance = this as T;
+    }
+
+    public class PersistentSingleton<T> : Singleton<T> where T : Component
+    {
+        public override void InitializeSingleton()
         {
             if (_instance == null)
             {
-                _instance = FindAnyObjectByType<T>();
-                if (_instance == null)
-                {
-                    GameObject obj = new();
-                    obj.name = typeof(T).Name + "AutoCreated";
-                    _instance = obj.AddComponent<T>();
-                }
+                _instance = this as T;
+
+                transform.SetParent(null);
+                DontDestroyOnLoad(gameObject);
             }
-
-            return _instance;
+            else if (_instance != this)
+                Destroy(gameObject);
         }
     }
 
-    protected static T _instance { get; set; }
-
-    protected virtual void OnDestroy()
+    public class RegulatorSingleton<T> : Singleton<T> where T : Component
     {
-        if (_instance == this)
-            _instance = null;
-    }
+        public float _initializationTime { get; private set; }
 
-    public virtual void Awake()
-    {
-        if (Application.isPlaying)
-            InitializeSingleton();
-    }
-
-    protected virtual void InitializeSingleton() =>
-        _instance = this as T;
-}
-
-public class PersistentSingleton<T> : Singleton<T> where T : Component
-{
-    protected override void InitializeSingleton()
-    {
-        if (_instance == null)
+        public override void InitializeSingleton()
         {
-            _instance = this as T;
+            _initializationTime = Time.time;
 
-            transform.SetParent(null);
             DontDestroyOnLoad(gameObject);
+
+            foreach (var old in FindExistingInstances())
+                if (old._initializationTime < this._initializationTime)
+                    Destroy(old.gameObject);
+
+            if (_instance == null)
+                _instance = this as T;
         }
-        else if (_instance != this)
-            Destroy(gameObject);
-    }
-}
 
-public class RegulatorSingleton<T> : Singleton<T> where T : Component
-{
-    protected float _initializationTime { get; private set; }
-
-    protected override void InitializeSingleton()
-    {
-        _initializationTime = Time.time;
-
-        DontDestroyOnLoad(gameObject);
-
-        foreach (var old in FindExistingInstances())
-            if (old._initializationTime < this._initializationTime)
-                Destroy(old.gameObject);
-
-        if (_instance == null)
-            _instance = this as T;
-    }
-
-    private IEnumerable<RegulatorSingleton<T>> FindExistingInstances()
-    {
-        T[] oldInstances = FindObjectsByType<T>(FindObjectsSortMode.None);
-        foreach (var old in oldInstances)
-            yield return old.GetComponent<RegulatorSingleton<T>>();
+        private IEnumerable<RegulatorSingleton<T>> FindExistingInstances()
+        {
+            T[] oldInstances = FindObjectsByType<T>(FindObjectsSortMode.None);
+            foreach (var old in oldInstances)
+                yield return old.GetComponent<RegulatorSingleton<T>>();
+        }
     }
 }
