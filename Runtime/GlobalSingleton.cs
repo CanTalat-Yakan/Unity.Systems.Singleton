74c2014d2e6bcec5d9b20e3366db1b144c461f3b
CanTalat-Yakan <can.talat.yakan@gmail.com>
2026-02-05 16:09:24 +0100

    ⠀⠀⠀⠀⠀

--- Runtime/GlobalSingleton.cs
+++ Runtime/GlobalSingleton.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace UnityEssentials
@@ -42,8 +43,8 @@ namespace UnityEssentials
                 if (!GlobalSingletonBootstrap.CanUseUnityObjectApi)
                     return null;
 
-                // Try to rebind to an existing instance (runtime-safe API).
-                s_instance = FindExistingInstance(includeInactive: true);
+                // Rebind + dedupe first.
+                s_instance = FindAndDedupeExistingInstances();
                 if (s_instance != null)
                     return s_instance;
 
@@ -76,9 +77,15 @@ namespace UnityEssentials
 
         protected virtual void Awake()
         {
+            // Ensure the host visibility is correct for the current mode.
+            ApplyVisibility(gameObject);
+
             if (s_instance == null)
             {
-                s_instance = this as T;
+                // In case we survived a domain reload, prefer to rebind to the existing object
+                // (and make sure duplicates are removed).
+                s_instance = FindAndDedupeExistingInstances() ?? (this as T);
+
                 ConfigureRoot(gameObject);
 
                 if (Application.isPlaying)
@@ -86,9 +93,15 @@ namespace UnityEssentials
                     transform.SetParent(null);
                     DontDestroyOnLoad(gameObject);
                 }
+
+                // If we ended up not being the chosen instance, delete this object.
+                if (s_instance != this)
+                    DestroyImmediate(gameObject);
             }
             else if (s_instance != this)
+            {
                 DestroyImmediate(gameObject);
+            }
         }
 
         protected virtual void OnDestroy()
@@ -97,10 +110,72 @@ namespace UnityEssentials
                 s_instance = null;
         }
 
-        private static void ConfigureRoot(GameObject go) =>
+        private static void ConfigureRoot(GameObject go)
+        {
+            // Never save this object into scenes.
+            // Also control visibility: hidden in edit mode, visible during play mode.
             go.hideFlags = HideFlags.DontSave;
+            ApplyVisibility(go);
+        }
 
-        private static T FindExistingInstance(bool includeInactive) =>
-            FindFirstObjectByType<T>(includeInactive? FindObjectsInactive.Include : FindObjectsInactive.Exclude);
+        private static void ApplyVisibility(GameObject go)
+        {
+            if (go == null)
+                return;
+
+            // In Edit Mode, keep it out of the hierarchy/inspector. In Play Mode, show it.
+            if (Application.isPlaying)
+                go.hideFlags &= ~HideFlags.HideInHierarchy;
+            else
+                go.hideFlags |= HideFlags.HideInHierarchy;
+        }
+
+
+        private static T FindAndDedupeExistingInstances()
+        {
+            // Note: "hidden" objects are still returned by FindObjectsByType; hideFlags don't exclude them.
+            // We include inactive so we can rebind even if it was disabled.
+            var instances = FindObjectsByType<T>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            if (instances == null || instances.Length == 0)
+                return null;
+
+            // Prefer an instance that already carries our marker suffix.
+            T chosen = null;
+            for (var i = 0; i < instances.Length; i++)
+            {
+                var inst = instances[i];
+                if (inst == null)
+                    continue;
+
+                if (inst.gameObject != null && inst.gameObject.name.EndsWith(AutoSuffix))
+                {
+                    chosen = inst;
+                    break;
+                }
+            }
+
+            chosen ??= instances[0];
+
+            // Remove duplicates.
+            for (var i = 0; i < instances.Length; i++)
+            {
+                var inst = instances[i];
+                if (inst == null || inst == chosen)
+                    continue;
+
+                // Keep behavior consistent in edit-time.
+                DestroyImmediate(inst.gameObject);
+            }
+
+            // Bring flags and (if needed) persistence back in line.
+            if (chosen != null)
+            {
+                ConfigureRoot(chosen.gameObject);
+                if (Application.isPlaying)
+                    DontDestroyOnLoad(chosen.gameObject);
+            }
+
+            return chosen;
+        }
     }
 }
